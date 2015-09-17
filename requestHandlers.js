59686431495e4ab51f7eb199e03c9898538ea2a5
MMaurel <mmaurel@telecomdesign.fr>
2015-09-17 10:40:16 +0200

    Adding rights on files

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,9 @@
-var querystring = require("querystring"),
-    fs = require("fs"),
-    formidable = require("formidable");
+var querystring = require('querystring'),
+    fs = require('fs'),
+    formidable = require('formidable');
 
 function start(response) {
-  console.log("Le gestionnaire 'start' est appelé.");
+  console.log('Le gestionnaire "start" est appelé.');
 
   var body = '<html>'+
     '<head>'+
@@ -19,44 +19,44 @@ function start(response) {
     '</body>'+
     '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(body);
     response.end();
 }
 
 function upload(response, request) {
-  console.log("Le gestionnaire 'upload' est appelé.");
+  console.log('Le gestionnaire "upload" est appelé.');
 
   var form = new formidable.IncomingForm();
-  console.log("Récupération des éléments reçus");
+  console.log('Récupération des éléments reçus');
   form.parse(request, function(error, fields, files) {
-    console.log("Traitement terminé");
+    console.log('Traitement terminé');
 
     /* En cas d'erreur sous Windows :
        tentative d'écrasement d'un fichier existant */
-    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+    fs.rename(files.upload.path, '/tmp/test.png', function(err) {
       if (err) {
-        fs.unlink("/tmp/test.png");
-        fs.rename(files.upload.path, "/tmp/test.png");
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
       }
     });
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Image reçue :<br/>");
-    response.write("<img src='/show' />");
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('Image reçue :<br/>');
+    response.write('<img src="/show" />');
     response.end();
   });
 }
 
 function show(response) {
-  console.log("Le gestionnaire 'show' est appelé.");
-  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+  console.log('Le gestionnaire "show" est appelé.');
+  fs.readFile('/tmp/test.png', 'binary', function(error, file) {
     if(error) {
-      response.writeHead(500, {"Content-Type": "text/plain"});
-      response.write(error + "\n");
+      response.writeHead(500, {'Content-Type': 'text/plain'});
+      response.write(error + '\n');
       response.end();
     } else {
-      response.writeHead(200, {"Content-Type": "image/png"});
-      response.write(file, "binary");
+      response.writeHead(200, {'Content-Type': 'image/png'});
+      response.write(file, 'binary');
       response.end();
     }
   });
